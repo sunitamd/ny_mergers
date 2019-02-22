b266c11f546d0741d44de20756e70f35978737a5
Alan Z Chen <alanzchen7@gmail.com>
2019-02-21 19:53:39 -0500

    update shell to run explore/ny_sid_supp.do

--- batch/stata.sh
+++ batch/stata.sh
@@ -2,6 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
+#SBATCH --mem=256GB
 #SBATCH --mail-type=NONE
 #SBATCH --mail-user=alanzchen@nyu.edu
 #SBATCH --output=$scratch/slurm/slurm_%j.out
@@ -11,10 +12,10 @@ module load stata/15
 RUNDIR=$scratch/slurm/run-${SLURM_JOB_ID/.*}
 mkdir -p $RUNDIR
 
+
 # Command line  arguments
-ARGS=\"$1\"
+ARGS=$1
 
 
 # Run job
-stata -b do $ARGS
-echo "do $ARGS"
+stata -b do "explore/ny_sid_supp.do" $ARGS
