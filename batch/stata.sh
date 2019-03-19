10f92ca714c5ea8ecc63ac2d292bd4ba851d6aa8
Alan Z Chen <alanzchen7@gmail.com>
2019-03-19 12:50:25 -0400

    remove sbatch options that seem to cause crash

--- batch/stata.sh
+++ batch/stata.sh
@@ -4,23 +4,19 @@
 #SBATCH --mem=32GB
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mail-type=END
-#SBATCH --mail-user=alanzchen@nyu.edu
-#SBATCH --output=/gpfs/scratch/$USER/slurm/slurm_%j.out
+#SBATCH --cpus-per-task=1
 
+# Load modules
 module purge
 module load stata/15
 
-
-PROJDIR=/gpfs/home/$USER/ny_mergers
-
+# Setup repo directory
+REPO=/gpfs/home/$USER/ny_mergers
 
 # Command line  arguments
-export STATA_DO="stata -b do \"$PROJDIR/$1\""
+export STATA_DO="stata -b do \"$REPO/$1\""
 shift
 export ARGS=$@
-
 
 # Run program
 echo "$STATA_DO $ARGS"
