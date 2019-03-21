02f800ecc61b7ecd7141be0003c42a80377d0183
Alan Z Chen <alanzchen7@gmail.com>
2019-03-21 15:36:54 -0400

    add scatter showing hhi in first and last year

--- code/build/hcup/explore/hhi_hospital.r
+++ code/build/hcup/explore/hhi_hospital.r
@@ -13,11 +13,25 @@ na_2006 <- nrow(filter(ptemp, is.na(year_2006)))
 na_2012 <- nrow(filter(ptemp, is.na(year_2012)))
 caption_na <- paste0('Note: ', na_2006, ', ', na_2012, ' hospitals not observed in 2006, 2012, respectively.')
 
-ggplot(ptemp, aes(year_2006, year_2012)) +
-	geom_point() +
+ggplot(ptemp, aes(x=year_2006, y=year_2012)) +
+	geom_point(alpha=0.7) +
 	geom_abline(slope=1, intercept=0, linetype='dashed', col='grey40') +
 	xlim(c(0,1.0)) +
 	ylim(c(0,1.0)) +
 	labs(title="Hospital-level HHI", x="HHI (2006)", y="HHI (2012)", caption=caption_na)
-ggsave('outputs/hhi_hospital_scatter.pdf', device='pdf')
+ggsave('outputs/hhi_hospital_scatter1.pdf', device='pdf')
 
+ptemp <- hhi %>%
+	group_by(ahaid) %>%
+		mutate(hhi1=first(hhi_hosp, order_by=year), hhi2=last(hhi_hosp, order_by=year)) %>%
+		ungroup() %>%
+	select(ahaid, hhi1, hhi2) %>%
+	distinct()
+
+ggplot(ptemp, aes(x=hhi1, y=hhi2)) +
+	geom_point(alpha=0.7) +
+	geom_abline(slope=1, intercept=0, linetype='dashed', col='grey40') +
+	xlim(c(0,1.0)) +
+	ylim(c(0,1.0)) +
+	labs(title="Hospital-level HHI", x="HHI (first year)", y="HHI (last year)", caption='First & last year represent first and last year of observed data')
+ggsave('outputs/hhi_hospital_scatter2.pdf', device='pdf')
