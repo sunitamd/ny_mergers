c7bf2d2eefd54b4f967a4b2df4f5387ad1e0f61c
Alan Z Chen <alanzchen7@gmail.com>
2019-03-26 12:21:38 -0400

    save plot

--- code/build/hcup/explore/zipcode_visitlink.r
+++ code/build/hcup/explore/zipcode_visitlink.r
@@ -26,3 +26,4 @@ ggplot(temp, aes(ahaid_cd, rate, col=log(ds))) +
     facet_wrap(~var, ncol=1, labeller=labeller(var=var_labels)) +
     scale_color_gradientn('Discharges (log)', colors=cols)
     labs(x='AHAID', y='Rate', title='Rate of discharges for:', caption='Rates & Discharges: total across all years')
+ggsave('outputs/zipcode_visitlink.pdf', device='pdf')
