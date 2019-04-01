2639bbbee11336e5dcfc91df2dc9f2d2a3b7adfc
Alan Z Chen <alanzchen7@gmail.com>
2019-04-01 17:27:50 -0400

    add kmeans clustering for mdc & drg codes

--- code/build/hcup/explore/drg_mdc.r
+++ code/build/hcup/explore/drg_mdc.r
@@ -4,6 +4,8 @@
 
 library(haven)
 library(tidyverse)
+library(factoextra)
+library(gridExtra)
 library(RColorBrewer)
 
 ############################################
@@ -31,15 +33,21 @@ pay_cols <- brewer.pal(6, 'Paired')
 # DATA
 ############################################
 
-if (sample) {
-    hcup <- read_dta(paste0(proj_dir, 'ny_mergers/data_sidclean/sid_work/ny_sid_0612_supp_sample.dta'))
+if (Sys.info()['sysname'] == 'unix') {
+    if (sample) {
+        hcup <- read_dta(paste0(proj_dir, 'ny_mergers/data_sidclean/sid_work/ny_sid_0612_supp_sample.dta'))
+    } else {
+        hcup <- readRDS(paste0(proj_dir, 'ny_mergers/data_sidclean/sid_work/ny_sid_0612_supp_sample.rds'))
+    }
 } else {
-    hcup <- read_dta(paste0(proj_dir, 'ny_mergers/data_sidclean/sid_work/ny_sid_0612_supp_sample.dta'))
+    hcup <- readRDS('dump/ny_sid_0612_supp.rds')
 }
 
 ############################################
 # RUN PROGRAM
 ############################################
+
+plot.list <- list(length=2)
 
 # Collapse mdc codes by payer
 mdc <- hcup %>%
@@ -54,7 +62,7 @@ ptemp <- ggplot(mdc, aes(reorder(mdc, -Medicaid), ds, fill=pay1)) +
     geom_col() +
     scale_fill_manual('Payer', values=pay_cols) +
     labs(x='MDC', y='Discharges', caption='Discharges for all HCUP years')
-ggsave(filename='outputs/mdc_drg/mdc.pdf', plot=ptemp, device='pdf')
+plot.list[[1]] <- ptemp
 
 # Collapse drg codes by payer
 drg <- hcup %>%
@@ -74,5 +82,62 @@ ptemp <- ggplot(drg %>% filter(Medicaid>medicaid_top10), aes(reorder(drg, -Medic
     scale_fill_manual('Payer', values=pay_cols) +
     labs(x='DRG Codes', y='Discharges', caption='Discharges for all HCUP years') +
     theme(axis.ticks.x=element_blank(), axis.text.x=element_blank())
-ggsave(filename='outputs/mdc_drg/drg.pdf', plot=ptemp, device='pdf')
+plot.list[[2]] <- ptemp
 
+plots <- do.call(marrangeGrob, c(plot.list, list(nrow=1, ncol=1)))
+ggsave("outputs/mdc_drg/plots.pdf", plots, device='pdf')
+
+
+############################################
+# K-means clustering
+############################################
+
+# 1. select features
+# 2. scale & standardize features
+# 3. calculate distance matrix / decide which type of distance metric to use
+# 4. kmeans clustering
+
+# MDC
+############################################
+df <- scale(mdc_spread)
+
+set.seed(8)
+
+mdc_ks <- lapply(2:15, function(k, .df=df) {
+    kmeans(.df[,-1], k, nstart=25)
+})
+mdc_wss <- lapply(mdc_ks, function(kmean) kmean$tot.withinss)
+
+mdc_plots <- lapply(1:5, function(i, .kmeans=mdc_ks, .df=df) {
+    fviz_cluster(.kmeans[[i]], geom='point', data=.df) + ggtitle(paste0('k=',i+1))
+})
+grid.arrange(mdc_plots[[1]], mdc_plots[[2]], mdc_plots[[3]], mdc_plots[[4]], mdc_plots[[5]], nrow=3)
+
+plot(2:15, mdc_wss,
+       type="b", pch = 19, frame = FALSE, 
+       xlab="Number of clusters K",
+       ylab="Total within-clusters sum of squares")
+
+
+# DRG Codes
+############################################
+
+df <- scale(drg_spread[,-1])
+rownames(df) <- drg_spread[,1][[1]]
+
+set.seed(8)
+
+drg_ks <- lapply(2:15, function(k, .df=df) {
+    kmeans(.df, k, nstart=25)
+})
+drg_wss <- lapply(drg_ks, function(kmean) kmean$tot.withinss)
+
+drg_plots <- lapply(1:5, function(i, .kmeans=drg_ks, .df=df) {
+    fviz_cluster(.kmeans[[i]], geom='point', data=.df) + ggtitle(paste0('k=',i+1))
+})
+grid.arrange(drg_plots[[1]], drg_plots[[2]], drg_plots[[3]], drg_plots[[4]], drg_plots[[5]], nrow=3)
+
+plot(2:15, drg_wss,
+       type="b", pch = 19, frame = FALSE, 
+       xlab="Number of clusters K",
+       ylab="Total within-clusters sum of squares")
